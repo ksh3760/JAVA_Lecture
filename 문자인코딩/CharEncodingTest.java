c45448e41e2044bfa8c8917c6864ab2a5147a8ff
ksh3760 <63641306+ksh3760@users.noreply.github.com>
2021-01-19 00:26:39 +0900

    add java file

---
+++ 문자인코딩/CharEncodingTest.java
@@ -0,0 +1,24 @@
+package 스트림연습;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class CharEncodingTest {
+	public static void main(String[] args) throws IOException {
+		// 문자 인코딩
+		
+		File fileDir = new File("input.txt");
+		BufferedReader in = new BufferedReader(new InputStreamReader(
+				new FileInputStream(fileDir), "UTF-8"));
+		
+		String str;
+		
+		while((str = in.readLine()) != null) {
+			System.out.println(str);
+		}
+		
+	}
+}
