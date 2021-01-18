39009faa319a3efd5b227ea2b6a15c2612e3293d
ksh3760 <63641306+ksh3760@users.noreply.github.com>
2021-01-19 00:11:13 +0900

    add java file

---
+++ 파일 입출력/imageFileCopy.java
@@ -0,0 +1,30 @@
+package 스트림연습;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.Scanner;
+
+public class imageFileCopy {
+    public static void main(String[] args) throws IOException {
+    	Scanner scn = new Scanner(System.in);
+        System.out.print("원본 파일 명을 입력하세요 : ");
+        String inputFileName = scn.next();
+
+        System.out.print("복사하는 파일 명을 입력하세요 : ");
+        String outputFileName = scn.next();
+
+        try (InputStream inputStream = new FileInputStream(inputFileName);
+                OutputStream outputStream = new FileOutputStream(outputFileName)) {
+                int c;
+
+                while ((c = inputStream.read()) != -1) {
+                    outputStream.write(c);
+                }
+            }
+            System.out.println(inputFileName + "을 " + outputFileName + "(으)로 복사하였습니다.");
+
+   }
+}
