7b20953b4d34b0c1b76a8aa019ce59403116894f
ksh3760 <63641306+ksh3760@users.noreply.github.com>
2021-01-27 23:11:36 +0900

    app java file

---
+++ 네트워크/ParseURLExample.java
@@ -0,0 +1,22 @@
+package 네트워크연습;
+// URL 클래스
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class ParseURLExample {
+
+	public static void main(String[] args) throws Exception {
+		URL myURL = new URL("http://java.sun.com:80/docs/books/tutorial" + "/index.html?name=database#TOP");	// 객체생성
+		
+		System.out.println("protocol  = " + myURL.getProtocol());
+		System.out.println("authority = " + myURL.getAuthority());
+		System.out.println("host      = " + myURL.getHost());
+		System.out.println("port      = " + myURL.getPort());
+		System.out.println("path      = " + myURL.getPath());
+		System.out.println("query     = " + myURL.getQuery());
+		System.out.println("filename  = " + myURL.getFile());
+		System.out.println("ref       = " + myURL.getRef());
+		
+	}
+
+}
