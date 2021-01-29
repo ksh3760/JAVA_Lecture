d62e175758c5e8805ecab20ca601c54969bbc6e5
ksh3760 <63641306+ksh3760@users.noreply.github.com>
2021-01-29 23:19:43 +0900

    add java file
    서버로 부터 날짜정보를 얻는 클라이언트

---
+++ 네트워크/DateClient.java
@@ -0,0 +1,22 @@
+// 날짜 클라이언트
+// 서버로 부터 날짜 정보를 받는다.
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+
+public class DataClient {
+	public static void main(String[] args) throws IOException {
+		Socket s = new Socket("localhost", 9100);	// 포트 9100버능로 연결 시도
+		
+		BufferedReader input = new BufferedReader(
+				new InputStreamReader(s.getInputStream()));	// 소켓으로부터 스트림을 얻는다.
+		
+		String res = input.readLine();
+		System.out.println(res);
+		
+		System.exit(0);
+		
+	}
+}
